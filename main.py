c1bb8e8703f7592e228afe647d7fbd36bd718c30
Mitchell Vivian <mitchell.j.vivian@gmail.com>
2021-09-27 11:59:45 -0700

    Fix get visitor total

--- main.py
+++ main.py
@@ -45,15 +45,23 @@ def getversionid():
     versionid = os.getenv('GAE_VERSION')
     return str(versionid)
 
-@app.route('visitors')
+@app.route('/visitors')
 def getVisitors():
     addVisitor()
     ent = dataclient.key('data', 'visitors')
-    total = dataclient.get(key=client)
+    total = dataclient.get(key=ent)
     if total:
         return 'Total visitors: ' + str(total['total'])
     else:
         return 'Total broke!'
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python3_app]
+# [END gae_python38_app]
 
 def addVisitor():
     ent = dataclient.key('data', 'visitors')
@@ -65,11 +73,3 @@ def addVisitor():
         total = datastore.Entity(key=ent)
         total['total'] = 0
         dataclient.put(total)
-
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
